0c0bc5d741449ddb16686e2c3e1e6136e262fbc5
JosueS02 <josueamerjsv5@gmail.com>
2025-04-30 23:03:56 -0600

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 
-const apiUrl = 'mysql://avnadmin:AVNS_OxghixWankmVvlCh1sG@mysql-28221c6d-jsv02.j.aivencloud.com:14234/defaultdb?ssl-mode=REQUIRED';
+const apiUrl = '/api/f1';
 const driverForm = document.getElementById('driverForm');
 const driversTable = document.getElementById('driversTable');
 const submitBtn = document.getElementById('submitBtn');
